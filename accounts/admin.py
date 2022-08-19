a75f122be9a4987b755e39bfa28f9aebf4083051
pemidi <rashidi.peyman@gmail.com>
2022-08-19 04:42:01 +0430

    feat: add User to admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username')

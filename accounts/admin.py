e5c610f22063a2c864ec4a6cc00accb194b6d790
pemidi <rashidi.peyman@gmail.com>
2022-08-25 10:41:38 +0430

    feat: add AdminModels, Add Inlines

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
-
-# Register your models here.
 from django.contrib.auth import get_user_model
+
+from accounts.models import Portfolio, UserSkill
 
 User = get_user_model()
 
 
+class PortfolioInline(admin.TabularInline):
+    model = Portfolio
+
+
+class UserSkillInline(admin.TabularInline):
+    model = UserSkill
+
+
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ['user', 'title']
+
+
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'username')
+    list_display = ('id', 'username', 'get_full_name')
+
+    inlines = [PortfolioInline, UserSkillInline]

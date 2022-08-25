fa4e2baaa8f630e7c3305fb9e5ffec22fda29e31
pemidi <rashidi.peyman@gmail.com>
2022-08-25 05:56:03 +0430

    add pagination class

--- jobs/views.py
+++ jobs/views.py
@@ -1,10 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 
 from jobs.models import Project
+from jobs.pagination import StandardSizePagination
 from jobs.serializers import ProjectSerializer
 
 
 class ProjectViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    pagination_class = StandardSizePagination
     lookup_field = 'pk'

05de9cdbb824bfac0624d2e559c83dc3cbccb1d3
pemidi <rashidi.peyman@gmail.com>
2022-10-30 16:03:50 +0330

    fix: fix ProjectSerializer, add user serializer, add get_status_display

--- jobs/api/serializers.py
+++ jobs/api/serializers.py
@@ -1,9 +1,13 @@
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
 from jobs.models import Project
+from accounts.api.serializers import UserReadOnlySerializer
 
 
 class ProjectSerializer(ModelSerializer):
+    user = UserReadOnlySerializer()
+    status = serializers.CharField(source='get_status_display')
 
     class Meta:
         model = Project
